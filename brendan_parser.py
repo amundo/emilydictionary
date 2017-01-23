b72066a684d6b1555b1b9d910c9a4d8e2209acf9
Patrick Hall <pjh@umail.ucsb.edu>
2017-01-23 14:47:35 -0500

    Create brendan_parser.py

---
+++ brendan_parser.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+from lxml import html
+import lxml.html.builder as E
+
+doc = html.parse('dictionary-11-08-2016.html')
+body = doc.xpath('/html/*')[1]
+
+with open('crappy_version.txt', 'w') as f:
+    for item in body.cssselect('div > p.EntryParagraph'):
+        stuff = {cls: (item.cssselect('span.{}'.format(cls)) or [E.SPAN("XXX")])[0].text_content() for cls in ("Lexeme", "Glossn", "Definitionn")}
+        for k, v in stuff.iteritems():
+            print("{}: {}".format(k, v.replace('\n', '')), file=f)
+print('\n', file=f)
